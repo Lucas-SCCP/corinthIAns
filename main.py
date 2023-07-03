3992b029d7da140256442d4cfe80ad70158e2445
Lucas Rodrigues <lucas.alves@rentcars.com>
2023-07-02 22:37:56 -0300

    Adicionado consulta a API OpenAI

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
+import config
 import openai
+
 openai.organization = "org-2IJ8iShyCp0Qn6PFM7pI3shY"
-openai.api_key = os.getenv("OPENAI_API_KEY")
-openai.Model.list()
+openai.api_key = config.OPENAI_API_KEY
+response = openai.Model.list()
+
+print(response)
